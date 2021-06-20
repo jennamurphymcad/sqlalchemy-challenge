8487a3a2b5be5dfa08d019fc889cb5de70c56820
Jenna Murphy <jennadeemurphy@gmail.com>
2021-06-20 14:46:46 -0500

    added final comments

--- app.py
+++ app.py
@@ -46,30 +46,36 @@ def percipitations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #set results equaly to query
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > '2016-08-23').order_by(Measurement.date).all()
 
+    #close session
     session.close()
 
-    
+    #set results into dictionary and append into a empty list
     all_prcp = []
     for date, prcp in results:
         prcp_dict = {}
         prcp_dict[date] = prcp
         all_prcp.append(prcp_dict)
 
+    #return jsonify result list
     return jsonify(all_prcp)
 
-#Query all stations
+#Query all stations into a list
 @app.route("/api/v1.0/stations")
 def stations():
 
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #set results equaly to query
     results = session.query(Measurement.station).group_by(Measurement.station).all()
 
+    #close session
     session.close()
 
+    #return jsonify results
     return jsonify(results)
 
 
@@ -80,25 +86,31 @@ def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #set results equal to query 
     results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > '2016-08-23').filter(Measurement.station == 'USC00519281').order_by(Measurement.date).all()
 
+    #close session
     session.close()
 
+    #return jsonify result
     return jsonify(results)
 
 
-#Query Min, Max Avg by Start Date
+#Query Min, Max Avg by Start Date provided by user in search bar
 @app.route("/api/v1.0/<search_date>")
 def start_date(search_date):
     
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #set result equal to query
     result = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs),\
                           func.avg(Measurement.tobs)).filter(Measurement.date >= search_date).all()
     
+    #close session
     session.close()
 
+    #add results to a dictionary to make result readout a bit more clear
     result_list = []
     for date, min, max, avg in result:
         result_dict = {}
@@ -108,6 +120,7 @@ def start_date(search_date):
         result_dict["Avg Temp"] = avg
         result_list.append(result_dict)
 
+    #return jsonify result
     return jsonify(result_list)
 
 
@@ -118,12 +131,15 @@ def search_date(start=None, end=None):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #set result equal to query
     result = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs),\
                           func.avg(Measurement.tobs)).filter(Measurement.date >= start, Measurement.date <=\
                           end).all()
     
+    #close session
     session.close()
 
+    #add results to a dictionary to make result readout a bit more clear
     result_list = []
     for date, min, max, avg in result:
         result_dict = {}
@@ -134,6 +150,7 @@ def search_date(start=None, end=None):
         result_dict["Avg Temp"] = avg
         result_list.append(result_dict)
 
+    #return jsonify result
     return jsonify(result_list)
 
 
